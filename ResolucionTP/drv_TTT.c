0831aae1927d2a2b6181a1a3bd398d2c23dea723
gaston segura <54334534+gastonsegura2908@users.noreply.github.com>
2024-06-15 16:51:28 -0300

    Update drv_TTT.c

--- ResolucionTP/drv_TTT.c
+++ ResolucionTP/drv_TTT.c
@@ -1,6 +1,3 @@
-// Parto de drv3 para realizar las modificaciones a read y write para ver qué hace
-// estoy yendo en la dirección de clipboard.c ..!!
-
 #include <linux/module.h>
 #include <linux/version.h>
 #include <linux/kernel.h>
@@ -10,67 +7,70 @@
 #include <linux/device.h>
 #include <linux/cdev.h>
 #include <linux/uaccess.h>
+#include <linux/gpio.h>
+#include <linux/interrupt.h>
 
-static dev_t first; 		// Global variable for the first device number
-static struct cdev c_dev; 	// Global variable for the character device structure
-static struct class *cl; 	// Global variable for the device class
+#define GPIO_PIN_1 17 // GPIO17 (Pin 11 en la placa)
+#define GPIO_PIN_2 27 // GPIO27 (Pin 13 en la placa)
 
-static char c;
+static dev_t first;        
+static struct cdev c_dev;  
+static struct class *cl;   
+
+static char selected_gpio = 1; // 1 para GPIO_PIN_1, 2 para GPIO_PIN_2
 
 static int my_open(struct inode *i, struct file *f)
 {
     printk(KERN_INFO "SdeC_drvTTT: open()\n");
     return 0;
 }
+
 static int my_close(struct inode *i, struct file *f)
 {
     printk(KERN_INFO "SdeC_drvTTT: close()\n");
     return 0;
 }
 
-// ssize_t resulta ser una palabra con signo.
-// Por lo tanto, puede ocurrir que devuelva un número negativo. Esto sería un error. 
-// Pero un valor de retorno no negativo tiene un significado adicional. 
-// Para my_read sería el número de bytes leídos
-
-// Cuando hago un $ cat /dev/SdeC_drv3, se convoca a my_read.!!
-// my_read lee "len" bytes, los guarda en "buf" y devuelve la cantidad leida, que puede
-// ser menor, pero nunca mayor que len.
-
-// En SdeC_drv3, devuelve cero. Dado que es un archivo, esto significa no hay mas datos ó EOF.
-// Lo que tendría que ocurrir es que el device escriba sobre buf para que el usuario pueda 
-// obtener una lectura no nula.
-
 static ssize_t my_read(struct file *f, char __user *buf, size_t len, loff_t *off)
 {
+    int gpio_val;
+    char value;
     printk(KERN_INFO "SdeC_drvTTT: read()\n");
 
-    if (*off == 0)
-    {
-        if ( copy_to_user(buf, &c, 1) != 0)
-            return -EFAULT;
-        else
-        {
-            (*off)++;
-            return 1;
-        }
-    }
-    else
+    if (*off > 0)
         return 0;
 
-}
+    if (selected_gpio == 1)
+        gpio_val = gpio_get_value(GPIO_PIN_1);
+    else
+        gpio_val = gpio_get_value(GPIO_PIN_2);
 
-// my_write escribe "len" bytes en "buf" y devuelve la cantidad escrita, que debe ser igual "len".
-// Cuando hago un $ echo "bla bla bla..." > /dev/SdeC_drv3, se convoca a my_write.!!
+    value = gpio_val + '0';
+
+    if (copy_to_user(buf, &value, 1))
+        return -EFAULT;
+
+    (*off)++;
+    return 1;
+}
 
 static ssize_t my_write(struct file *f, const char __user *buf, size_t len, loff_t *off)
 {
+    char kbuf;
+
     printk(KERN_INFO "SdeC_drvTTT: write()\n");
 
-    if ( copy_from_user(&c, buf + len - 1, 1) != 0 )
+    if (copy_from_user(&kbuf, buf, 1))
         return -EFAULT;
+
+    if (kbuf == '1')
+        selected_gpio = 1;
+    else if (kbuf == '2')
+        selected_gpio = 2;
     else
-        return len;
+        return -EINVAL; // Invalid argument
+
+    return len;
 }
 
 static struct file_operations pugs_fops =
@@ -94,20 +94,22 @@ static int __init drv_TTT_init(void) /* Constructor */
         return ret;
     }
 
-    if (IS_ERR(cl = class_create(THIS_MODULE, "SdeC_drive")))
+    cl = class_create("SdeC_drive");
+    if (IS_ERR(cl))
     {
         unregister_chrdev_region(first, 1);
         return PTR_ERR(cl);
     }
 
-    if (IS_ERR(dev_ret = device_create(cl, NULL, first, NULL, "SdeC_drvTTT")))
+    dev_ret = device_create(cl, NULL, first, NULL, "SdeC_drvTTT");
+    if (IS_ERR(dev_ret))
     {
         class_destroy(cl);
         unregister_chrdev_region(first, 1);
         return PTR_ERR(dev_ret);
     }
 
-    cdev_init(&c_dev, &pugs_fops); // inicializamos las operaciones a realizar
+    cdev_init(&c_dev, &pugs_fops);
 
     if ((ret = cdev_add(&c_dev, first, 1)) < 0)
     {
@@ -116,6 +118,19 @@ static int __init drv_TTT_init(void) /* Constructor */
         unregister_chrdev_region(first, 1);
         return ret;
     }
+
+    if (!gpio_is_valid(GPIO_PIN_1) || !gpio_is_valid(GPIO_PIN_2))
+    {
+        printk(KERN_INFO "Invalid GPIO pins\n");
+        return -ENODEV;
+    }
+
+    gpio_request(GPIO_PIN_1, "GPIO_PIN_1");
+    gpio_direction_input(GPIO_PIN_1);
+
+    gpio_request(GPIO_PIN_2, "GPIO_PIN_2");
+    gpio_direction_input(GPIO_PIN_2);
+
     return 0;
 }
 
@@ -125,6 +140,10 @@ static void __exit drv_TTT_exit(void) /* Destructor */
     device_destroy(cl, first);
     class_destroy(cl);
     unregister_chrdev_region(first, 1);
+
+    gpio_free(GPIO_PIN_1);
+    gpio_free(GPIO_PIN_2);
+
     printk(KERN_INFO "SdeC_drvTTT: dice EL trabajo ha sido finalizado!!\n");
 }
 
